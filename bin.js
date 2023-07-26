d0e7d0aae3b48808d7c0175efb490e86b948ccdd
Jam19th <jam19th@gmail.com>
2023-07-25 22:21:15 -0700

    Finished

--- bin.js
+++ bin.js
@@ -3,7 +3,7 @@ const spawn = require('cross-spawn')
 const open = require('open')
 const { exec } = require('child_process')
 exec('npx babel ./src --out-dir=./dist', () => {
-    spawn('npx', [ 'babel', './src', '--out-dir=./dist', '--watch' ], { stdio: 'inherit' });
+    spawn('npx.cmd', [ 'babel', './src', '--out-dir=./dist', '--watch' ], { stdio: 'inherit' });
     open('./public/index.html')
     console.log('Watching for changes in "./src"')
 })
